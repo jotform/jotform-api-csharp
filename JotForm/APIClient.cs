44703a87307b4bedb3630afce20ac2dd51e91d2d
ceren <ceren@jotform.com>
2013-06-28 13:57:09 +0300

    c# client added

---
+++ JotForm/APIClient.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JotForm
+{
+    public class APIClient
+    {
+        private string apiKey;
+        private string username;
+        private bool debugMode;
+
+        private string baseURL = "http://api.jotform.com/";
+        private string apiVersion = "v1";
+
+        public APIClient(String apiKey, bool debugMode=false)
+        {
+            this.apiKey = apiKey;
+
+            this.debugMode = debugMode;
+        }
+
+        public JObject executeHttpRequest(string path, NameValueCollection parameters, string method)
+        {
+            //WebRequest req = WebRequest.Create("http://requestb.in/oanh1hoa");
+
+            if (method == "GET" && parameters != null)
+            {
+                path = path + "?" + ToQueryString(parameters);
+            }
+
+            WebRequest req = WebRequest.Create(this.baseURL + this.apiVersion + path);
+            req.Method = method;
+
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("apiKey:" + apiKey );
+            req.Headers = headers;
+            
+
+            if (method == "POST")
+            {
+                var data = Encoding.UTF8.GetBytes(ToQueryString(parameters));
+                
+                req.ContentLength = data.Length;
+                req.ContentType = "application/x-www-form-urlencoded";
+
+                Stream dataStream = req.GetRequestStream();
+
+                dataStream.Write(data, 0, data.Length);
+                dataStream.Close();
+            }
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            Stream answerStream = resp.GetResponseStream();
+            String answerString = new StreamReader(answerStream).ReadToEnd();
+
+            var responseCode = JObject.Parse(answerString)["responseCode"];
+
+            if (responseCode.ToString() != "200")
+            {
+                if (responseCode.ToString() == "401")
+                {
+                    throw new JotformException("Unauthozired API call");
+                }
+                else if (responseCode.ToString() == "404")
+                {
+                    throw new JotformException(JObject.Parse(answerString)["message"].ToString());
+                }
+                else if (responseCode.ToString() == "503")
+                {
+                    throw new JotformException("Service is unavaible, rate limits etc exceeded!");
+                }
+            }
+
+            return JObject.Parse(answerString);
+        }
+
+        private string ToQueryString(NameValueCollection nvc)
+        {
+            var array = (from key in nvc.AllKeys
+                         from value in nvc.GetValues(key)
+                         select string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(value)))
+                .ToArray();
+
+            return string.Join("&", array);
+        }
+
+        public JObject executeGetRequest(string path, NameValueCollection parameters=null)
+        {
+            return executeHttpRequest(path, parameters, "GET");
+        }
+
+        public JObject executePostRequest(string path, NameValueCollection parameters=null)
+        {
+            return executeHttpRequest(path, parameters, "POST");
+        }
+        
+        public JObject getUser()
+        {
+            return executeGetRequest("/user");
+        }
+
+        public JObject getUsage()
+        {
+            return executeGetRequest("/user/usage");
+        }
+
+        public JObject getForms()
+        {
+            return executeGetRequest("/user/forms");
+        }
+
+        public JObject getSubmissions()
+        {
+            return executeGetRequest("/user/submissions");
+        }
+
+        public JObject getSubusers()
+        {
+            return executeGetRequest("/user/subusers");
+        }
+
+        public JObject getFolders()
+        {
+            return executeGetRequest("/user/folders");
+        }
+
+        public JObject getReports()
+        {
+            return executeGetRequest("/user/reports");
+        }
+
+        public JObject getSettings()
+        {
+            return executeGetRequest("/user/settings");
+        }
+
+        public JObject getHistory()
+        {
+            return executeGetRequest("/user/history");
+        }
+
+        public JObject getForm(long formID)
+        {
+            return executeGetRequest("/form/" + formID.ToString());
+        }
+
+        public JObject getFormQuestions(long formID)
+        {
+            return executeGetRequest("/form/" + formID.ToString() + "/questions");
+        }
+
+        public JObject getFormQuestion(long formID, long qid)
+        {
+            return executeGetRequest("/form/" + formID.ToString() + "/question/" + qid.ToString());
+        }
+
+        public JObject getFormSubmissons(long formID)
+        {
+            return executeGetRequest("/form/" + formID.ToString() + "/submissions");
+        }
+        
+        public JObject createFormSubmissions(long formID, string qid, string submission)
+        {
+            var data = new NameValueCollection();
+            data.Add(qid, submission);
+
+            return executePostRequest("/form/" + formID.ToString() + "/submissions", data);
+        }
+        
+        public JObject getFormFiles(long formID)
+        {
+            return executeGetRequest("/form/" + formID.ToString() + "/files");
+        }
+
+        public JObject getFormWebhooks(long formID)
+        {
+            return executeGetRequest("/form/" + formID.ToString() + "/webhooks");
+        }
+
+        public JObject createFormWebhook(long formID, string webhookURL)
+        {
+            var data = new NameValueCollection();
+            data.Add("webhookURL", webhookURL);
+            return executePostRequest("/form/" + formID.ToString() + "/webhooks", data);
+        }        
+
+        public JObject getSubmission(long sid)
+        {
+            return executeGetRequest("/submission/" + sid.ToString());
+        }
+
+        public JObject getReport(long reportID)
+        {
+            return executeGetRequest("/report/" + reportID.ToString());
+        }
+
+        public JObject getFolder(string folderID)
+        {
+            return executeGetRequest("/folder/" + folderID);
+        }
+    }
+
+    public class JotformException : System.Exception
+    {
+        public JotformException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ JotFormTest/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JotForm;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JotFormTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new JotForm.APIClient("YOUR API CODE");
+
+            // Print new submission id
+            var user = client.getUser();
+
+            var submissions = client.getSubmissions()["content"];
+
+            var fs = from sub in submissions
+                     where (string)sub["new"] == "1"
+                     select sub.Value<string>("id");
+
+            foreach (var item in fs)
+            {
+                Console.Out.Write(item.ToString());
+            }
+            
+
+            // Print all forms of the user
+
+            var forms = client.getForms()["content"];
+
+            var formTitles = from form in forms
+                             select form.Value<string>("title");
+
+            foreach (var item in formTitles)
+            {
+                Console.Out.WriteLine(item);
+            }
+            
+            // Get latest submissions of the user
+
+            var allsubmissions = client.getSubmissions()["content"];
+
+            var createdat = (from submission in allsubmissions
+                            select submission.Value<string>("created_at")).ToArray();
+
+            for (int i = 0; i < createdat.Length; i++)
+            {
+                Console.Out.WriteLine(createdat[i]);
+
+                var answer = from sub in allsubmissions
+                             from ans in sub["answers"]
+                             where sub.Value<string>("created_at") == createdat[i]
+                             select ans;
+
+                foreach (var item in answer)
+                {
+                    Console.Out.WriteLine(item);
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
